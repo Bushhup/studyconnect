0a25c90c3e8273dd3fa96c6c511060d1cd5e66d9
S MD Shabuddin Osaid <shabuddinaw@gmail.com>
2026-01-30 08:36:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1138506612-7420e",
+  "appId": "1:140002479123:web:2c5afeffb60386e523c3e6",
+  "apiKey": "AIzaSyBJLIZ1vhIxhW05Q4Ybnw7B6HsUBiEw7To",
+  "authDomain": "studio-1138506612-7420e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "140002479123"
+};
